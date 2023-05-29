8628b99eac101e5fa12f1cc12e61f680855d49ed
Tetsuya Yamamoto <tetsuya.yamamoto@aibliss.co.jp>
2023-05-29 17:44:22 +0900

    Add mirDeep2 to NGS container

--- images/ngs/entrypoint.sh
+++ images/ngs/entrypoint.sh
@@ -16,6 +16,11 @@ chmod 0440 /etc/sudoers.d/user
 export HOME=/home/user
 echo "export PS1='[docker \h \W]\$ '" >> $HOME/.bashrc
 
+export PATH=$PATH:/usr/local/mirdeep2/bin
+export PERL5LIB=/usr/local/mirdeep2/lib/perl5
+export PERL_MB_OPT="--install_base /home/user/perl5"
+export PERL_MM_OPT="INSTALL_BASE=/home/user/perl5"
+
 sudo /etc/init.d/ssh start
 exec /usr/sbin/gosu user nohup jupyter lab --ip=0.0.0.0 --port 8888 --no-browser --NotebookApp.token='' > ~/jupyter.log 2> ~/jupyter.err &
 
